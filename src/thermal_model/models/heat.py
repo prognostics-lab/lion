770fc96a509862e85705d69845aacfb8bacfeaee
Cristóbal Allendes <callendes.molina@gmail.com>
2024-02-15 21:45:31 -0300

    Added a saturation to the generated heat

--- src/thermal_model/models/heat.py
+++ src/thermal_model/models/heat.py
@@ -1,5 +1,7 @@
+import numpy as np
+
 from thermal_model.models.ehc import entropy_change
 
 
 def generated_heat_from_current(current, temperature, soc, internal_resistance):
-    return current ** 2 * internal_resistance - current * temperature * entropy_change(soc)
+    return np.clip(current ** 2 * internal_resistance - current * temperature * entropy_change(soc), 0, np.inf)
