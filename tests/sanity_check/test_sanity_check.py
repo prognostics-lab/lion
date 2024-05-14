bbd44de4e1cb368b0d0afb4ef09712bc485e7dd5
Cristóbal Allendes <callendes.molina@gmail.com>
2024-05-14 14:37:39 -0400

    Added some prints

--- tests/sanity_check/test_sanity_check.py
+++ tests/sanity_check/test_sanity_check.py
@@ -6,9 +6,18 @@ import pandas as pd
 from scipy import optimize
 import pytest
 
-from thermal_model.estimation import lti_from_data, TargetParams
+from thermal_model.estimation import lti_from_data
 from thermal_model.estimation.models import generate_evaluation
 from thermal_model.logger import LOGGER, setup_logger
+
+
+_EPSILON = 1e-9
+
+
+@pytest.fixture(scope="session", autouse=True)
+def logger():
+    setup_logger(quiet=False, debug=True, verbose=True)
+    return LOGGER
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -35,8 +44,8 @@ def data():
 
 
 @pytest.fixture(scope="session", autouse=True)
-def optimize_results(data):
-    y, u, t, x0, real_params = data
+def optimize_results(logger, data):
+    y, u, t, x0, _ = data
 
     (A, B, C, _), params = lti_from_data(
         y,
@@ -46,23 +55,23 @@ def optimize_results(data):
         0,
         0,
         optimizer_kwargs={
-            "method": "Nelder-Mead",
-            "tol": 1e-18,
             "fn": optimize.minimize,
+            "method": "SLSQP",
+            "tol": 1e-18,
             "options": {
-                "maxiter": 1e1,
+                "maxiter": 1e2,
             },
+            # "fn": optimize.least_squares,
             # "method": "trf",
             # "verbose": 2,
             # "bounds": (_EPSILON, np.inf),
-            # "fn": optimize.least_squares,
         },
     )
-    LOGGER.info("\n")
-    LOGGER.info(f"Final parameters: {params}")
-    LOGGER.info(f"A = \n{A}")
-    LOGGER.info(f"B = \n{B}")
-    LOGGER.info(f"C = \n{C}")
+    logger.info("\n")
+    logger.info(f"Final parameters: {params}")
+    logger.info(f"A = \n{A}")
+    logger.info(f"B = \n{B}")
+    logger.info(f"C = \n{C}")
     print(params)
     return A, B, C, params, data
 
@@ -88,6 +97,8 @@ def test_mse(optimize_results, acceptable_mse):
 
 def test_cp(optimize_results, epsilon):
     *_, params, (*_, real_params) = optimize_results
+    print(f"Real parameters: {real_params}")
+    print(f"Parameters: {params}")
     abs_error = np.abs(params.cp - real_params["cp"])
     assert abs_error <= epsilon, f"Parameter 'cp' is below acceptable ({
         abs_error:.4e} > {epsilon:.4e})"
@@ -95,6 +106,8 @@ def test_cp(optimize_results, epsilon):
 
 def test_cair(optimize_results, epsilon):
     *_, params, (*_, real_params) = optimize_results
+    print(f"Real parameters: {real_params}")
+    print(f"Parameters: {params}")
     abs_error = np.abs(params.cair - real_params["cair"])
     assert abs_error <= epsilon, f"Parameter 'cair' is below acceptable ({
         abs_error:.4e} > {epsilon:.4e})"
@@ -102,6 +115,8 @@ def test_cair(optimize_results, epsilon):
 
 def test_rin(optimize_results, epsilon):
     *_, params, (*_, real_params) = optimize_results
+    print(f"Real parameters: {real_params}")
+    print(f"Parameters: {params}")
     abs_error = np.abs(params.rin - real_params["rin"])
     assert abs_error <= epsilon, f"Parameter 'rin' is below acceptable ({
         abs_error:.4e} > {epsilon:.4e})"
@@ -109,6 +124,8 @@ def test_rin(optimize_results, epsilon):
 
 def test_rout(optimize_results, epsilon):
     *_, params, (*_, real_params) = optimize_results
+    print(f"Real parameters: {real_params}")
+    print(f"Parameters: {params}")
     abs_error = np.abs(params.rout - real_params["rout"])
     assert abs_error <= epsilon, f"Parameter 'rout' is below acceptable ({
         abs_error:.4e} > {epsilon:.4e})"
@@ -116,6 +133,8 @@ def test_rout(optimize_results, epsilon):
 
 def test_rair(optimize_results, epsilon):
     *_, params, (*_, real_params) = optimize_results
+    print(f"Real parameters: {real_params}")
+    print(f"Parameters: {params}")
     abs_error = np.abs(params.rair - real_params["rair"])
     assert abs_error <= epsilon, f"Parameter 'rair' is below acceptable ({
         abs_error:.4e} > {epsilon:.4e})"
