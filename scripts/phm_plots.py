f2d9235098f0d82546ce27927992fefe107bdad0
Cristóbal Allendes <callendes.molina@gmail.com>
2024-06-03 16:33:06 -0400

    Added plots of the models to the PHM plots

--- scripts/phm_plots.py
+++ scripts/phm_plots.py
@@ -3,19 +3,34 @@ import sys
 import pathlib
 import argparse
 
+import pandas as pd
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 src_path = pathlib.Path.joinpath(pathlib.Path(os.getcwd()), "src")
 sys.path.append(str(src_path))
+# pylint: disable=import-error
 from thermal_model.estimation import lti_from_data
 from thermal_model.estimation.models import generate_evaluation
-from thermal_model.models import generated_heat_from_current, EhcParams, OcvParams, ThermalParams, calculate_soc_coeff, calculate_soc_with_temperature, calculate_ocv_temperature, calculate_ocv, calculate_ocv
+from thermal_model.models import (
+    generated_heat_from_current,
+    EhcParams,
+    OcvParams,
+    ThermalParams,
+    calculate_soc_coeff,
+    calculate_soc_with_temperature,
+    calculate_ocv_temperature,
+    calculate_ocv,
+    calculate_ocv,
+)
+# pylint: enable=import-error
 
 
 # Argument parser
-parser = argparse.ArgumentParser(prog="phm_plots", description="generator for PHM plots")
+parser = argparse.ArgumentParser(
+    prog="phm_plots", description="generator for PHM plots"
+)
 parser.add_argument(
     "-s",
     "--showfig",
@@ -34,18 +49,19 @@ parser.add_argument(
 SMALL_SIZE = 8
 MEDIUM_SIZE = 10
 BIGGER_SIZE = 12
+DEFAULT_FIGSIZE = (3.38765625, 3)
 
-plt.rc("text", usetex=True)
-plt.rc("font", family="Times")
-plt.rc("font", size=MEDIUM_SIZE)
-plt.rc("axes", titlesize=MEDIUM_SIZE)
-plt.rc("axes", labelsize=MEDIUM_SIZE)
-plt.rc("xtick", labelsize=SMALL_SIZE)
-plt.rc("ytick", labelsize=SMALL_SIZE)
-plt.rc("legend", fontsize=SMALL_SIZE)
-plt.rc("figure", titlesize=MEDIUM_SIZE)
-plt.rc("figure", figsize=(3.38765625, 3))
-plt.rc("lines", linewidth=1)
+mpl.rcParams["text.usetex"] = True
+mpl.rcParams["font.family"] = "Times"
+mpl.rcParams["font.size"] = MEDIUM_SIZE
+mpl.rcParams["axes.titlesize"] = MEDIUM_SIZE
+mpl.rcParams["axes.labelsize"] = MEDIUM_SIZE
+mpl.rcParams["ytick.labelsize"] = SMALL_SIZE
+mpl.rcParams["ytick.labelsize"] = SMALL_SIZE
+mpl.rcParams["legend.fontsize"] = SMALL_SIZE
+mpl.rcParams["figure.titlesize"] = MEDIUM_SIZE
+mpl.rcParams["figure.figsize"] = DEFAULT_FIGSIZE
+mpl.rcParams["lines.linewidth"] = 1
 
 plt.style.use("tableau-colorblind10")
 
@@ -101,7 +117,11 @@ def generate_soc_shift_plt(savefig):
     for t in temperatures:
         soc_t = calculate_soc_with_temperature(soc, t, TH_PARAMS)
         if t == temperatures[0] or t == temperatures[-1]:
-            ax.plot(100 * soc, 100 * soc_t, label=f"T° = {t:.0f}\\,K ({kelvin_to_celsius(t):.0f}\\,°C)")
+            ax.plot(
+                100 * soc,
+                100 * soc_t,
+                label=f"T° = {t:.0f}\\,K ({kelvin_to_celsius(t):.0f}\\,°C)",
+            )
         else:
             ax.plot(100 * soc, 100 * soc_t, alpha=0.1, color="#aaa")
     ax.plot(100 * soc, 100 * soc, linestyle="--", color="#000", label="Reference T°")
@@ -114,11 +134,13 @@ def generate_soc_shift_plt(savefig):
     fig.tight_layout()
 
     if savefig:
-        fig.savefig(os.path.join(THEORY_IMG_DIR, f"soc_shift.{SAVE_FMT}"), **SAVEFIG_PARAMS)
+        fig.savefig(
+            os.path.join(THEORY_IMG_DIR, f"soc_shift.{SAVE_FMT}"), **SAVEFIG_PARAMS
+        )
 
 
 def generate_ocv_plt(savefig):
-    fig, ax  = plt.subplots()
+    fig, ax = plt.subplots()
     soc = np.linspace(0.01, 1, 100)
     temperatures = np.linspace(celsius_to_kelvin(-40), celsius_to_kelvin(100), 100)
 
@@ -126,10 +148,20 @@ def generate_ocv_plt(savefig):
         soc_t = calculate_soc_with_temperature(soc, t, TH_PARAMS)
         ocv = calculate_ocv_temperature(soc_t, t, OCV_PARAMS)
         if t == temperatures[0] or t == temperatures[-1]:
-            ax.plot(100 * soc, ocv, label=f"T° = {t:.0f}\\,K ({kelvin_to_celsius(t):.0f}\\,°C)")
+            ax.plot(
+                100 * soc,
+                ocv,
+                label=f"T° = {t:.0f}\\,K ({kelvin_to_celsius(t):.0f}\\,°C)",
+            )
         else:
             ax.plot(100 * soc, ocv, alpha=0.1, color="#aaa")
-    ax.plot(100 * soc, calculate_ocv(soc), linestyle="--", color="#000", label="Reference T°")
+    ax.plot(
+        100 * soc,
+        calculate_ocv(soc),
+        linestyle="--",
+        color="#000",
+        label="Reference T°",
+    )
     ax.set_title("Effect of temperature on OCV curve according to model")
     ax.grid(alpha=0.25)
     ax.set_xlabel(r"$\mathrm{SoC}_0$ (\%)")
@@ -139,19 +171,87 @@ def generate_ocv_plt(savefig):
     fig.tight_layout()
 
     if savefig:
-        fig.savefig(os.path.join(THEORY_IMG_DIR, f"ocv_curve.{SAVE_FMT}"), **SAVEFIG_PARAMS)
+        fig.savefig(
+            os.path.join(THEORY_IMG_DIR, f"ocv_curve.{SAVE_FMT}"), **SAVEFIG_PARAMS
+        )
+
+
+def generate_air_theory_plt(savefig):
+    figsize = (DEFAULT_FIGSIZE[0], 9)
+    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=figsize)
+
+    df_real = pd.read_csv(os.path.join("examples", "air_effect", "data_air_real.csv"))
+    df_est = pd.read_csv(os.path.join("examples", "air_effect", "data_air_est.csv"))
+
+    real_time = df_real["time"].to_numpy() / 3600
+    est_time = df_est["time"].to_numpy() / 3600
+
+    ax1.plot(real_time, df_real["sf_temp"], label="Expected", linestyle="--")
+    ax1.plot(est_time, df_est["sf_temp"], label="Obtained")
+    ax2.plot(real_time, 1e3 * df_real["q_gen"], label="Expected", linestyle="--")
+    ax2.plot(est_time, 1e3 * df_est["q_gen"], label="Obtained")
+
+    ax1.set_title("Surface temperature")
+    ax2.set_title("Generated heat")
+    ax1.grid(alpha=0.25)
+    ax2.grid(alpha=0.25)
+    ax1.legend()
+    ax2.legend()
+
+    ax2.set_xlabel("Time (h)")
+    ax1.set_ylabel("Temperature (°C)")
+    ax2.set_ylabel("Heat (mW)")
+
+    fig.tight_layout()
+    if savefig:
+        fig.savefig(
+            os.path.join(THEORY_IMG_DIR, f"air_curves.{SAVE_FMT}"), **SAVEFIG_PARAMS
+        )
+
+
+def generate_noair_theory_plt(savefig):
+    figsize = (DEFAULT_FIGSIZE[0], 9)
+    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=figsize)
+
+    df_real = pd.read_csv(os.path.join("examples", "air_effect", "data_noair_real.csv"))
+    df_est = pd.read_csv(os.path.join("examples", "air_effect", "data_noair_est.csv"))
+
+    real_time = df_real["time"].to_numpy() / 3600
+    est_time = df_est["time"].to_numpy() / 3600
+
+    ax1.plot(real_time, df_real["sf_temp"], label="Expected", linestyle="--")
+    ax1.plot(est_time, df_est["sf_temp"], label="Obtained")
+    ax2.plot(real_time, 1e3 * df_real["q_gen"], label="Expected", linestyle="--")
+    ax2.plot(est_time, 1e3 * df_est["q_gen"], label="Obtained")
+
+    ax1.set_title("Surface temperature")
+    ax2.set_title("Generated heat")
+    ax1.grid(alpha=0.25)
+    ax2.grid(alpha=0.25)
+    ax1.legend()
+    ax2.legend()
+
+    ax2.set_xlabel("Time (h)")
+    ax1.set_ylabel("Temperature (°C)")
+    ax2.set_ylabel("Heat (mW)")
+
+    fig.tight_layout()
+    if savefig:
+        fig.savefig(
+            os.path.join(THEORY_IMG_DIR, f"noair_curves.{SAVE_FMT}"), **SAVEFIG_PARAMS
+        )
 
 
 def main(savefig=False, showfig=False):
     generate_soc_shift_plt(savefig)
     generate_ocv_plt(savefig)
+    generate_air_theory_plt(savefig)
+    generate_noair_theory_plt(savefig)
 
     if showfig:
         plt.show()
 
 
-
 if __name__ == "__main__":
     args = parser.parse_args()
     main(savefig=args.savefig, showfig=args.showfig)
-
