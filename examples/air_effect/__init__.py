45498ce7c620ae6b13ad559d80a5f01cc3733153
Cristóbal Allendes <callendes.molina@gmail.com>
2024-06-03 16:32:46 -0400

    Made the data generation use some sensor noise, and made the example call MATLAB for data generation and model evaluation

--- examples/air_effect/__init__.py
+++ examples/air_effect/__init__.py
@@ -1,9 +1,23 @@
-from .estimate_parameters import main as _estimate_parameters_main
-from .plot_results import main as _plot_results_main
+import subprocess as sp
+
+# pylint: disable=import-error
+from thermal_model.logger import LOGGER
+# pylint: enable=import-error
+
+from .estimate_parameters import main as _main
 
 
-def main(plot=True):
-    if plot:
-        _plot_results_main()
-    else:
-        _estimate_parameters_main()
+def main():
+    LOGGER.info("Generating simulated data")
+    sp.run(["matlab", "-nodisplay", "-batch", "matlab.project.loadProject('battery_temperature_modelling.prj');", "gen_data.m"])
+
+    LOGGER.info("Running parameter estimation on simulated data")
+    _main()
+
+    LOGGER.info("Evaluating air estimation")
+    sp.run(["matlab", "-nodisplay", "-batch", "matlab.project.loadProject('battery_temperature_modelling.prj');", "eval_air_estimation.m"])
+
+    LOGGER.info("Evaluating no-air estimation")
+    sp.run(["matlab", "-nodisplay", "-batch", "matlab.project.loadProject('battery_temperature_modelling.prj');", "eval_noair_estimation.m"])
+
+
