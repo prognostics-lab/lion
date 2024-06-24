1788c33c3f3feea951c21520aa77e65c9b2a223f
Cristóbal Allendes <callendes.molina@gmail.com>
2024-06-24 17:35:49 -0400

    sad

--- examples/air_effect/plot_results.py
+++ examples/air_effect/plot_results.py
@@ -2,7 +2,23 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
+def _plot(df):
+    fig, ax = plt.subplots(2, 1)
+
+    ax_t = ax[0].twinx()
+    ax[0].plot(df["time"], df["voltage"], label="Current")
+    ax_t.plot(df["time"], df["current"], label="Voltage")
+    ax[0].legend()
+
+    ax[1].plot(df["time"], df["amb_temp"], label="Ambient")
+    ax[1].plot(df_air["time"], df_air["sf_temp"], label="Surface")
+
+    fig.tight_layout()
+
+
 def plot_results():
-    pass
-:qa
+    df_air = pd.read_csv("examples/air_effect/sim2_air.csv")
+    df_noair = pd.read_csv("examples/air_effect/sim2_noair.csv")
+
+
 
