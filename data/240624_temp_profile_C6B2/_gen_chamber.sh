3bc86cd32f13eff6a1ea477aad609a05036cee2d
Cristóbal Allendes <callendes.molina@gmail.com>
2024-06-26 20:03:51 -0400

    Created a script to merge chamber data and generated out file

--- data/240624_temp_profile_C6B2/_gen_chamber.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-# Copy files to a temporary directory for modification
-mkdir .temp
-cp data/240624_temp_profile_C6B2/Data\ Files_06-25-2024-18-47-58/*.csv .temp/
-echo "DateTime,CSZ System,TEMPERATURE PV,TEMPERATURE SP,TEMPERATURE %Out,PRODUCT PV,PRODUCT SP,PRODUCT %Out," > data/240624_temp_profile_C6B2/chamber.csv
-
