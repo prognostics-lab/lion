4af7dbce4f2c6de1fac05624b5c4f1893d0c823a
Cristóbal Allendes <callendes.molina@gmail.com>
2024-07-31 12:49:40 -0400

    Added more error functionals

--- src/thermal_model/estimation/error.py
+++ src/thermal_model/estimation/error.py
@@ -27,7 +27,7 @@ def l2(expected, u, t, x0, **kwargs) -> Callable[[TargetParams], float]:
     return _generate_system
 
 
-def l2_simulation(expected, u, t, x0, engine, mdl, simin, initial_soc, **kwargs) -> Callable[[TargetParams], float]:
+def l2_simulation(expected, u, t, x0, engine, mdl, simin, initial_soc, gain=1, **kwargs) -> Callable[[TargetParams], float]:
     names = kwargs.get("outputs")
     only_sf = names is not None and names != "both"
     initial_conditions_dict = {
@@ -54,25 +54,21 @@ def l2_simulation(expected, u, t, x0, engine, mdl, simin, initial_soc, **kwargs)
         except ValueError:
             mse = error.conjugate().T @ error / len(t)
         print(mse)
-        return mse
+        return gain * mse
     return _generate_system
 
 
+def logl2(*args, **kwargs) -> Callable[[TargetParams], float]:
+    out_fn = l2(*args, **kwargs)
+    return lambda x: np.log(out_fn(x))
+
+
+def logl2_simulation(*args, **kwargs) -> Callable[[TargetParams], float]:
+    out_fn = l2_simulation(*args, **kwargs)
+    return lambda x: np.log(out_fn(x))
+
+
 def likelihood(expected, u, t, x0, sensor_cov, prior: Callable[[TargetParams], float] = None, **kwargs) -> Callable[[TargetParams], float]:
-    # if prior is None:
-    #     prior = lambda params: 1
-    # sensor_cov_det = np.linalg.det(sensor_cov)
-    # sensor_cov_inv = np.linalg.inv(sensor_cov)
-    # def _generate_system(params: TargetParams) -> float:
-    #     y = target_response(u, t, x0, params, **kwargs)
-    #     error = expected - y
-    #     rows, cols = error.shape
-    #     prod = 1
-    #     for e in error:
-    #         sqrt_term = (2 * np.pi)**cols * sensor_cov_det
-    #         prod *= np.exp(-0.5 * e @ sensor_cov_inv @ e.T) / np.sqrt(sqrt_term)
-    #     return 1 - prod * prior(params)
-    # return _generate_system
     if prior is None:
         prior = lambda params: 1
     if len(x0) != 1:
@@ -96,3 +92,42 @@ def likelihood(expected, u, t, x0, sensor_cov, prior: Callable[[TargetParams], f
             return -prior(params) * (exp_term / sqrt_term).prod()
     return _generate_system
 
+
+def likelihood_simulation(expected, u, t, x0, engine, mdl, simin, initial_soc, sensor_cov, prior: Callable[[TargetParams], float] = None, **kwargs) -> Callable[[TargetParams], float]:
+    names = kwargs.get("outputs")
+    only_sf = names is not None and names != "both"
+    initial_conditions_dict = {
+        "initial_soc": initial_soc,
+    }
+    if only_sf:
+        initial_conditions_dict["initial_in_temp"] = x0
+        expected = expected[1:]
+    else:
+        initial_conditions_dict["initial_in_temp"] = x0[0]
+        initial_conditions_dict["initial_air_temp"] = x0[1]
+    if prior is None:
+        prior = lambda params: 1
+    if len(x0) != 1:
+        sensor_cov_det = np.linalg.det(sensor_cov)
+        sensor_cov_inv = np.linalg.inv(sensor_cov)
+        def _generate_system(params: TargetParams) -> float:
+            params_dict = params._asdict()
+            simout = engine.py_evaluate_model(mdl, simin, params_dict, initial_conditions_dict)
+            obtained = np.array(simout)[:, 2]
+            error = expected - obtained
+            rows, cols = error.shape
+            exp_term = np.exp(-0.5 * np.diag(error.T @ error @ sensor_cov_inv).sum())
+            # sqrt_term = ((2 * np.pi) ** cols * sensor_cov_det) ** rows
+            sqrt_term = 1
+            return -prior(params) * exp_term  / np.sqrt(sqrt_term)
+    else:
+        sensor_cov_det = np.abs(sensor_cov)
+        def _generate_system(params: TargetParams) -> float:
+            params_dict = params._asdict()
+            simout = engine.py_evaluate_model(mdl, simin, params_dict, initial_conditions_dict)
+            obtained = np.array(simout)[:, 2]
+            error = expected - obtained
+            exp_term = np.exp(-error**2 / (2 * sensor_cov_det))
+            sqrt_term = np.sqrt(2 * np.pi * sensor_cov_det)
+            return -prior(params) * (exp_term / sqrt_term).prod()
+    return _generate_system
