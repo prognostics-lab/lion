84f13a8d3491c9f0a49ddab42180fa6097d5438a
Cristóbal Allendes <callendes.molina@gmail.com>
2024-09-02 14:18:20 -0400

    Added macros for GSL function calls

--- include/lionu/macros.h
+++ include/lionu/macros.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "log.h"
+#include <gsl/gsl_errno.h>
 #include <lion/status.h>
 
 #define LION_CALL(x, msg, ...)                                                 \
@@ -12,4 +13,16 @@
 #define LION_CALLDF(x, msg, ...)                                               \
   if (x != LION_STATUS_SUCCESS) {                                              \
     log_error(msg, #__VA_ARGS__);                                              \
+  }
+
+#define LION_GSL_CALL(x, msg, ...)                                             \
+  if (x != GSL_SUCCESS) {                                                      \
+    log_error(msg, #__VA_ARGS__);                                              \
+    return LION_STATUS_FAILURE;                                                \
+  }
+
+#define LION_GSL_CALLDF(x, msg, ...)                                           \
+  if (x != GSL_SUCCESS) {                                                      \
+    log_error(msg, #__VA_ARGS__);                                              \
+    return LION_STATUS_FAILURE;                                                \
   }
--- src/lion_utils/macros.h
+++ src/lion_utils/macros.h
@@ -2,6 +2,7 @@
 
 #include "vendor/log.h"
 
+#include <gsl/gsl_errno.h>
 #include <lion/status.h>
 #include <lionu/macros.h>
 
@@ -14,4 +15,16 @@
 #define LION_CALLDF_I(x, msg, ...)                                             \
   if (x != LION_STATUS_SUCCESS) {                                              \
     logi_error(msg, #__VA_ARGS__);                                             \
+  }
+
+#define LION_GSL_CALL_I(x, msg, ...)                                           \
+  if (x != GSL_SUCCESS) {                                                      \
+    logi_error(msg, #__VA_ARGS__);                                             \
+    return LION_STATUS_FAILURE;                                                \
+  }
+
+#define LION_GSL_CALLDF_I(x, msg, ...)                                         \
+  if (x != GSL_SUCCESS) {                                                      \
+    logi_error(msg, #__VA_ARGS__);                                             \
+    return LION_STATUS_FAILURE;                                                \
   }
