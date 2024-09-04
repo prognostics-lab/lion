7b93efe45e85e6540e38eb3e77f9def44fea1fbc
Cristóbal Allendes <callendes.molina@gmail.com>
2024-09-04 16:25:53 -0400

    Added a lot of states

--- include/lion/app.h
+++ include/lion/app.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <time.h>
 
+#include <gsl/gsl_min.h>
 #include <gsl/gsl_odeiv2.h>
 
 #include "params.h"
@@ -46,35 +47,42 @@ size_t heapinfo_count(lion_app_t *app);
 
 // Application declarations
 
-typedef enum terra_app_regimes {
-  TERRA_APP_ONLYSF = 0,
-  TERRA_APP_ONLYAIR = 1,
-  TERRA_APP_BOTH = 2,
-} terra_app_regimes_t;
+typedef enum lion_app_regime {
+  LION_APP_ONLYSF = 0,
+  LION_APP_ONLYAIR = 1,
+  LION_APP_BOTH = 2,
+} lion_app_regime_t;
 
-typedef enum terra_app_steppers {
-  TERRA_STEPPER_RK2 = 0,
-  TERRA_STEPPER_RK4 = 1,
-  TERRA_STEPPER_RKF45 = 2,
-  TERRA_STEPPER_RKCK = 3,
-  TERRA_STEPPER_RK8PD = 4,
-  TERRA_STEPPER_RK1IMP = 5,
-  TERRA_STEPPER_RK2IMP = 6,
-  TERRA_STEPPER_RK4IMP = 7,
-  TERRA_STEPPER_BSIMP = 8,
-  TERRA_STEPPER_MSADAMS = 9,
-  TERRA_STEPPER_MSBDF = 10,
-} terra_app_steppers_t;
+typedef enum lion_app_stepper {
+  LION_STEPPER_RK2 = 0,
+  LION_STEPPER_RK4 = 1,
+  LION_STEPPER_RKF45 = 2,
+  LION_STEPPER_RKCK = 3,
+  LION_STEPPER_RK8PD = 4,
+  LION_STEPPER_RK1IMP = 5,
+  LION_STEPPER_RK2IMP = 6,
+  LION_STEPPER_RK4IMP = 7,
+  LION_STEPPER_BSIMP = 8,
+  LION_STEPPER_MSADAMS = 9,
+  LION_STEPPER_MSBDF = 10,
+} lion_app_stepper_t;
 
-typedef struct terra_app_config {
+typedef enum lion_app_minimizer {
+  LION_MINIMIZER_GOLDENSECTION = 0,
+  LION_MINIMIZER_BRENT = 1,
+  LION_MINIMIZER_QUADGOLDEN = 2,
+} lion_app_minimizer_t;
+
+typedef struct lion_app_config {
   /* App metadata */
 
   const char *app_name;
 
   /* Simulation metadata */
 
-  terra_app_regimes_t sim_regime;
-  terra_app_steppers_t sim_stepper;
+  lion_app_regime_t sim_regime;
+  lion_app_stepper_t sim_stepper;
+  lion_app_minimizer_t sim_minimizer;
   double sim_time_seconds;
   double sim_step_seconds;
   double sim_epsabs;
@@ -87,15 +95,49 @@ typedef struct terra_app_config {
   int log_filelvl;
 } lion_app_config_t;
 
+typedef struct lion_app_state {
+  double time;
+
+  // System inputs
+  double power;
+  double ambient_temperature;
+
+  // Electrical state
+  double voltage;
+  double current;
+  double open_circuit_voltage;
+  double internal_resistance;
+
+  // Thermal state
+  double generated_heat;
+  double internal_temperature;
+  double surface_temperature;
+
+  // Charge state
+  double kappa;
+  double soc_nominal;
+  double capacity_nominal;
+  double soc_use;
+  double capacity_use;
+} lion_app_state_t;
+
 typedef struct lion_app {
   lion_app_config_t *conf;
   lion_params_t *params;
+  lion_app_state_t *state;
+
+  /* Hooks */
+
+  lion_status_t (*init_hook)(lion_app_t *app);
+  lion_status_t (*update_hook)(lion_app_t *app, uint64_t i);
+  lion_status_t (*finished_hook)(lion_app_t *app);
 
   /* Data handles */
 
   gsl_odeiv2_system sys;
   gsl_odeiv2_driver *driver;
   gsl_odeiv2_step_type *step_type;
+  gsl_min_fminimizer_type *minimizer;
 
   char log_filename[FILENAME_MAX + _LION_LOGFILE_MAX];
   FILE *log_file;
@@ -121,9 +163,11 @@ lion_params_t lion_app_params_default(void);
 
 lion_status_t lion_app_new(lion_app_config_t *conf, lion_params_t *params,
                            lion_app_t *out);
-lion_status_t lion_app_run(lion_app_t *app);
+lion_status_t lion_app_run(lion_app_t *app, lion_vector_t *power,
+                           lion_vector_t *ambient_temperature);
 
 int lion_app_should_close(lion_app_t *app);
+uint64_t lion_app_max_iters(lion_app_t *app);
 
 lion_status_t lion_app_cleanup(lion_app_t *app);
 
