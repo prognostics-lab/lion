55f3e252cec8f1b3ca4379bd7f67acb7c19cba39
Cristóbal Allendes <callendes.molina@gmail.com>
2024-09-04 16:26:40 -0400

    Added calls to the new features

--- src/lion_app/app_run.c
+++ src/lion_app/app_run.c
@@ -1,45 +1,47 @@
 #include <gsl/gsl_odeiv2.h>
 
 #include <lion/lion.h>
+#include <lion_utils/macros.h>
 #include <lion_utils/vendor/log.h>
 
 #include "app_run.h"
 
-lion_status_t lion_app_init(lion_app_t *app) {
+lion_status_t lion_app_init(lion_app_t *app, double initial_power,
+                            double initial_amb_temp) {
   // Simulation stepper configuration
   logi_debug("Configuring simulation stepper");
   switch (app->conf->sim_stepper) {
-  case TERRA_STEPPER_RK2:
+  case LION_STEPPER_RK2:
     app->step_type = gsl_odeiv2_step_rk2;
     break;
-  case TERRA_STEPPER_RK4:
+  case LION_STEPPER_RK4:
     app->step_type = gsl_odeiv2_step_rk4;
     break;
-  case TERRA_STEPPER_RKF45:
+  case LION_STEPPER_RKF45:
     app->step_type = gsl_odeiv2_step_rkf45;
     break;
-  case TERRA_STEPPER_RKCK:
+  case LION_STEPPER_RKCK:
     app->step_type = gsl_odeiv2_step_rkck;
     break;
-  case TERRA_STEPPER_RK8PD:
+  case LION_STEPPER_RK8PD:
     app->step_type = gsl_odeiv2_step_rk8pd;
     break;
-  case TERRA_STEPPER_RK1IMP:
+  case LION_STEPPER_RK1IMP:
     app->step_type = gsl_odeiv2_step_rk1imp;
     break;
-  case TERRA_STEPPER_RK2IMP:
+  case LION_STEPPER_RK2IMP:
     app->step_type = gsl_odeiv2_step_rk2imp;
     break;
-  case TERRA_STEPPER_RK4IMP:
+  case LION_STEPPER_RK4IMP:
     app->step_type = gsl_odeiv2_step_rk4imp;
     break;
-  case TERRA_STEPPER_BSIMP:
+  case LION_STEPPER_BSIMP:
     app->step_type = gsl_odeiv2_step_bsimp;
     break;
-  case TERRA_STEPPER_MSADAMS:
+  case LION_STEPPER_MSADAMS:
     app->step_type = gsl_odeiv2_step_msadams;
     break;
-  case TERRA_STEPPER_MSBDF:
+  case LION_STEPPER_MSBDF:
     app->step_type = gsl_odeiv2_step_msbdf;
     break;
   default:
@@ -47,8 +49,29 @@ lion_status_t lion_app_init(lion_app_t *app) {
     return LION_STATUS_FAILURE;
   }
 
+  // Optimization problem minimizer
+  logi_debug("Configuring optimization minimizer");
+  switch (app->conf->sim_minimizer) {
+  case LION_MINIMIZER_GOLDENSECTION:
+    app->minimizer = gsl_min_fminimizer_goldensection;
+  case LION_MINIMIZER_BRENT:
+    app->minimizer = gsl_min_fminimizer_brent;
+  case LION_MINIMIZER_QUADGOLDEN:
+    app->minimizer = gsl_min_fminimizer_quad_golden;
+  default:
+    logi_error("Desired minimizer not implemented");
+    return LION_STATUS_FAILURE;
+  }
+
   // System configuration
-  logi_debug("Configuring ode system");
+  logi_debug("Configuring initial state");
+  app->state->soc_nominal = app->params->init.initial_soc;
+  app->state->internal_temperature =
+      app->params->init.initial_internal_temperature;
+  app->state->time = 0.0;
+  LION_CALL_I(lion_slv_update(app, initial_power, initial_amb_temp),
+              "Failed spreading initial condition");
+  // TODO: Implement setting up ODE system
 
   // Set up driver
   logi_debug("Configuring simulation driver");
@@ -58,10 +81,39 @@ lion_status_t lion_app_init(lion_app_t *app) {
   return LION_STATUS_SUCCESS;
 }
 
-lion_status_t lion_app_simulate(lion_app_t *app) {
-  uint64_t max_iters =
-      (uint64_t)(app->conf->sim_time_seconds / app->conf->sim_step_seconds);
+lion_status_t lion_app_simulate(lion_app_t *app, lion_vector_t *power,
+                                lion_vector_t *amb_temp) {
+  uint64_t max_iters = lion_app_max_iters(app);
   logi_debug("Considering %d max iterations", max_iters);
+
+  if (app->init_hook != NULL) {
+    logi_debug("Found init hook");
+    LION_CALLDF_I(app->init_hook(app), "Failed calling init hook");
+  }
+
+  logi_debug("Starting iterations");
+  for (uint64_t i = 0; i < max_iters; i++) {
+    double partial_result[2];
+    LION_GSL_CALL_I(gsl_odeiv2_driver_apply_fixed_step(
+                        app->driver, &app->state->time,
+                        app->conf->sim_step_seconds, 1, partial_result),
+                    "Failed at step %d (t = %f)", i, app->state->time);
+    app->state->soc_nominal = partial_result[0];
+    app->state->internal_temperature = partial_result[1];
+    LION_CALL_I(lion_slv_update(app, lion_vector_get_d(app, power, i),
+                                lion_vector_get_d(app, amb_temp, i)),
+                "Failed updating state");
+
+    if (app->update_hook != NULL) {
+      LION_CALLDF_I(app->update_hook(app, i), "Failed calling update hook");
+    }
+  }
+
+  logi_debug("Finished iterations");
+  if (app->finished_hook != NULL) {
+    logi_debug("Found finished hook");
+    LION_CALLDF_I(app->finished_hook(app), "Failed calling finished hook");
+  }
   return LION_STATUS_SUCCESS;
 }
 
--- src/lion_app/app_run.h
+++ src/lion_app/app_run.h
@@ -5,8 +5,10 @@
 #include <lion/app.h>
 #include <lion/status.h>
 
-lion_status_t lion_app_init(lion_app_t *app);
-lion_status_t lion_app_simulate(lion_app_t *app);
+lion_status_t lion_app_init(lion_app_t *app, double initial_power,
+                            double initial_amb_temp);
+lion_status_t lion_app_simulate(lion_app_t *app, lion_vector_t *power,
+                                lion_vector_t *amb_temp);
 
 #ifndef NDEBUG
 lion_status_t lion_app_init_debug(lion_app_t *app);
