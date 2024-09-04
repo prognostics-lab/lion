f217d1d848052aa7f1efd7a8464cb1140a687780
Cristóbal Allendes <callendes.molina@gmail.com>
2024-09-04 16:25:21 -0400

    Started working on calculating the Jacobian

--- src/lion_app/solver/sys.c
+++ src/lion_app/solver/sys.c
@@ -1,10 +1,15 @@
 #include <gsl/gsl_errno.h>
+#include <gsl/gsl_matrix.h>
 
 #include <lion/params.h>
 #include <lion/solver/inputs.h>
 #include <lion/solver/sys.h>
+#include <lion_math/capacity.h>
+#include <lion_math/current.h>
+#include <lion_math/internal_resistance.h>
 #include <lion_math/dynamics/soc.h>
 #include <lion_math/dynamics/temperature.h>
+#include <lion_math/open_circuit.h>
 
 int lion_slv_system(double t, double state[], double out[], void *inputs) {
   /*
@@ -32,12 +37,31 @@ int lion_slv_system(double t, double state[], double out[], void *inputs) {
   return GSL_SUCCESS;
 }
 
-// TODO: Implement calculating the Jacobian
+double jac_0_1(double soc_use, double kappa, double r, double voc, double p,
+               double q_use, lion_params_t *params) {
+  double term1 = lion_current_grad_voc(p, voc, r, params);
+  double term2 = lion_voc_grad(soc_use, params);
+  return -term1 * term2 * kappa / q_use;
+}
+
 int lion_slv_jac(double t, double state[], double *dfdy, double dfdt[],
                  void *inputs) {
   lion_slv_inputs_t *p = inputs;
   double *sys_inputs = p->sys_inputs;
   lion_params_t *sys_params = p->sys_params;
+
+  double kappa = lion_kappa(state[1], sys_params);
+  double soc_use = lion_soc_usable(state[0], kappa, sys_params);
+  double rint = lion_resistance(sys_inputs[1], sys_params);
+
+  (void)t;
+  gsl_matrix_view dfdy_mat = gsl_matrix_view_array(dfdy, 2, 2);
+  gsl_matrix *m = &dfdy_mat.matrix;
+  gsl_matrix_set(m, 0, 0, 0.0);
+  gsl_matrix_set(m, 0, 1, 1.0);
+  dfdt[0] = 0.0;
+  dfdt[1] = 0.0;
+  return GSL_SUCCESS;
 
   return GSL_SUCCESS;
 }
