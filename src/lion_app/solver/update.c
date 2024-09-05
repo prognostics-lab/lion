d69a009edd0495d297e2c5603ad19582faeb7065
Cristóbal Allendes <callendes.molina@gmail.com>
2024-09-05 17:17:15 -0400

    Finished updating system state

--- src/lion_app/solver/update.c
+++ src/lion_app/solver/update.c
@@ -6,7 +6,9 @@
 
 lion_status_t lion_slv_update(lion_app_t *app) {
   // This function assumes app->state.{internal_temperature, soc_nominal}
-  // have been properly set, and spreads those initial values
+  // have been properly set, and spreads those initial values, and it also
+  // assumes that app->state.{power, ambient_temperature} have been filled with
+  // the corresponding input
   app->state.kappa = lion_kappa(app->state.internal_temperature, app->params);
   app->state.capacity_nominal = app->params->init.initial_capacity;
   app->state.soc_use =
@@ -16,6 +18,22 @@ lion_status_t lion_slv_update(lion_app_t *app) {
 
   app->state.open_circuit_voltage =
       lion_voc(app->state.soc_nominal, app->params);
-  // TODO: Add current calculation
+  app->state.current =
+      lion_current_optimize(app->sys_min, app->state.power, app->state.soc_use,
+                            app->state.open_circuit_voltage, app->state.current,
+                            app->conf->sim_epsabs, app->conf->sim_epsrel,
+                            app->conf->sim_min_max_iter, app->params);
+  app->state.internal_resistance =
+      lion_resistance(app->state.soc_use, app->state.current, app->params);
+  app->state.voltage = lion_voltage_from_current(
+      app->state.power, app->state.current, app->params);
+
+  app->state.ehc = lion_ehc(app->state.soc_use, app->params);
+  app->state.generated_heat = lion_generated_heat(
+      app->state.current, app->state.internal_temperature,
+      app->state.internal_resistance, app->state.ehc, app->params);
+  app->state.surface_temperature =
+      lion_surface_temperature(app->state.internal_temperature,
+                               app->state.ambient_temperature, app->params);
   return LION_STATUS_SUCCESS;
 }
