62fa2746f2131fc4e3a409b6eeffbfb04a56692d
Cristóbal Allendes <callendes.molina@gmail.com>
2024-09-05 21:43:52 -0400

    Added a calculation of the gradient of kappa

--- src/lion_math/capacity.h
+++ src/lion_math/capacity.h
@@ -3,6 +3,7 @@
 #include <lion/params.h>
 
 double lion_kappa(double internal_temperature, lion_params_t *params);
+double lion_kappa_grad(double internal_temperature, lion_params_t *params);
 double lion_soc_usable(double soc, double kappa, lion_params_t *params);
 double lion_capacity_usable(double capacity, double kappa,
                             lion_params_t *params);
