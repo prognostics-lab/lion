d04912850d394af4442ae741d6082a55818687d6
Cristóbal Allendes <callendes.molina@gmail.com>
2024-09-05 22:38:16 -0400

    Renamed the example

--- examples/lab_240716/main.c
+++ examples/lab_240716/main.c
@@ -2,6 +2,9 @@
 #include <lionu/log.h>
 
 int main(void) {
+  lion_vector_t power;
+  lion_vector_t amb_temp;
+
   log_info("Setting up configuration");
   lion_app_config_t conf = lion_app_config_default();
   conf.log_dir = "logs";
@@ -11,5 +14,6 @@ int main(void) {
   lion_params_t params = lion_params_default();
 
   lion_app_t app;
-  lion_app_new(&conf) return lion_app_run(&app);
+  lion_app_new(&conf, &params, &app);
+  return lion_app_run(&app, &power, &amb_temp);
 }
