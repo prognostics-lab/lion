1fdad2e01322261c5b05ecf2d68f2e15e7605ecd
Cristóbal Allendes <callendes.molina@gmail.com>
2024-09-12 14:36:52 -0300

    Added a test for the creation of vectors from csv files

--- tests/unittest/quick/test_vector.c
+++ tests/unittest/quick/test_vector.c
@@ -67,6 +67,28 @@ lion_status_t test_creation_from_array(lion_app_t *app) {
   return LION_STATUS_SUCCESS;
 }
 
+lion_status_t test_creation_from_csv(lion_app_t *app) {
+  const char *FILENAME =
+      LION_PROJECT_ROOT_DIR "tests/unittest/quick/vector.csv";
+  lion_vector_t vec;
+  double expected[] = {1.1, 2.2, 3.3, 4.4, 5.5};
+  size_t len = 5;
+
+  log_info("Creating vector");
+  LION_CALL(lion_vector_from_csv(app, FILENAME, sizeof(double), "%lf", &vec),
+            "Failed creating vector from csv");
+
+  log_debug("Checking creation result");
+  LION_ASSERT_EQI(vec.len, len);
+  LION_ASSERT_EQI(vec.capacity, len);
+  for (uint32_t i = 0; i < len; i++) {
+    LION_ASSERT_EQF(((double *)vec.data)[i], expected[i]);
+    LION_ASSERT_EQF(lion_vector_get_d(app, &vec, i), expected[i]);
+  }
+
+  return LION_STATUS_SUCCESS;
+}
+
 lion_status_t test_methods_get(lion_app_t *app) {
   lion_vector_t vec;
   uint32_t data[] = {7, 6, 5, 4, 3, 2, 1, 0};
@@ -244,6 +266,7 @@ int main(void) {
   LION_CALL_TEST(NULL, test_creation_zero);
   LION_CALL_TEST(NULL, test_creation_with_capacity);
   LION_CALL_TEST(NULL, test_creation_from_array);
+  LION_CALL_TEST(NULL, test_creation_from_csv);
 
   LION_CALL_TEST(NULL, test_methods_get);
   LION_CALL_TEST(NULL, test_methods_set);
