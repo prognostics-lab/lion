f611b0968e36f96db250d1aac71ec5b0f864e62b
Cristobal Allendes <callendes.molina@gmail.com>
2024-09-16 14:17:34 -0300

    Created files for some more tests

---
+++ tests/unittest/quick/test_math_algebraic.c
@@ -0,0 +1,5 @@
+// TODO: Add tests for algebraic equations
+
+#include <lion_utils/test.h>
+
+int main() { return TEST_PASS; }
---
+++ tests/unittest/quick/test_math_differential.c
@@ -0,0 +1,5 @@
+// TODO: Add tests for differential equations
+
+#include <lion_utils/test.h>
+
+int main() { return TEST_PASS; }
