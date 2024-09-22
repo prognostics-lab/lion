792f312653a8d7fc84323a0b8d63f6d51dd74f9f
Cristobal Allendes <callendes.molina@gmail.com>
2024-09-22 18:12:40 -0300

    Started working on documentation

---
+++ docs/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+
+from setuptools.config import read_configuration
+
+_metadata = read_configuration("../setup.cfg")["metadata"]
+project = _metadata["name"]
+author = _metadata["author"]
+release = _metadata["version"]
+copyright = f'2024, {author}'
+# project = 'lion'
+# author = 'CASE'
+# release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
