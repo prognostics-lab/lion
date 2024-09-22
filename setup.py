37c9793a884b4db1a596b81ba7fbd59fb312a834
Cristobal Allendes <callendes.molina@gmail.com>
2024-09-22 18:08:25 -0300

    Migrated configs from setup.cfg to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
 from setuptools import setup
 
-# The setup assumes the C library has been built and the .dll/.so files are located
-# in "bin" for Windows and "lib" for Linux
 
+# TODO: Set version dynamically
 if __name__ == "__main__":
     setup(
-        setup_requires=["cffi>=1.0.0"],
         cffi_modules=["cffi_build.py:ffi_builder"],
-        install_requires=["cffi>=1.0.0"],
     )
