a0d15bfcc2f3d5353fe006f1b289b03c5b2fa2c5
Cristóbal Allendes <callendes.molina@gmail.com>
2024-10-07 18:41:25 -0300

    Added some more stuff to the clean script

--- sys/clean.sh
+++ sys/clean.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo -e "\x1b[32;20mCleaning working directory\x1b[0m\n"
-rm -rf ./bin ./build ./lib ./pdb ./Testing ./logs
+rm -rf ./bin ./build ./lib ./pdb ./Testing ./logs ./pysrc/lion/_lion.*
