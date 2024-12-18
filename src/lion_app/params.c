48cc47e01e3a6b29e5e0bc3486afe530205d649d
Cristóbal Allendes <callendes.molina@gmail.com>
2024-12-18 18:34:06 -0300

    Changed the way the defaults are set

--- src/lion_app/params.c
+++ src/lion_app/params.c
@@ -2,54 +2,46 @@
 
 #include <lion/lion.h>
 
-const lion_params_t LION_APP_PARAMS_DEFAULT = {
-    .init =
-        {
-               .soc           = 0.1,
-               .temp_in       = 298.0,
-               .soh           = 1.0,
-               .capacity      = 14400.0,
-               .current_guess = 10.0,
-               },
-    .ehc =
-        {
-               .a     = 4e-5,
-               .b     = 5e-5,
-               .mu    = 0.4,
-               .kappa = 3,
-               .sigma = 0.05,
-               .l     = 7,
-               },
-    .ocv =
-        {
-               .alpha = 0.15,
-               .beta  = 17,
-               .gamma = 10.5,
-               .v0    = 4.14,
-               .vl    = 3.977,
-               },
-    .vft =
-        {
-               .k1   = -5.738,
-               .k2   = 209.9,
-               .tref = 298,
-               },
-    .temp =
-        {
-               .cp   = 100.0,
-               .rin  = 3.0,
-               .rout = 9.0,
-               },
-    .rint =
-        {
-               .model        = LION_RINT_MODEL_FIXED,
-               .params.fixed = 1.0,
-               },
+const lion_params_init_t LION_PARAMS_DEFAULT_INIT = {
+    .soc           = 0.1,
+    .temp_in       = 298.0,
+    .soh           = 1.0,
+    .capacity      = 14400.0,
+    .current_guess = 10.0,
 };
 
-const lion_params_rint_fixed_t LION_PARAMS_RINT_DEFAULT_FIXED = {.internal_resistance = 0.12};
+const lion_params_ehc_t LION_PARAMS_DEFAULT_EHC = {
+    .a     = 4e-5,
+    .b     = 5e-5,
+    .mu    = 0.4,
+    .kappa = 3,
+    .sigma = 0.05,
+    .l     = 7,
+};
 
-const lion_params_rint_polarization_t LION_PARAMS_RINT_DEFAULT_POLARIZATION = {
+const lion_params_ocv_t LION_PARAMS_DEFAULT_OCV = {
+    .alpha = 0.15,
+    .beta  = 17,
+    .gamma = 10.5,
+    .v0    = 4.14,
+    .vl    = 3.977,
+};
+
+const lion_params_vft_t LION_PARAMS_DEFAULT_VFT = {
+    .k1   = -5.738,
+    .k2   = 209.9,
+    .tref = 298,
+};
+
+const lion_params_temp_t LION_PARAMS_DEFAULT_TEMP = {
+    .cp   = 100.0,
+    .rin  = 3.0,
+    .rout = 9.0,
+};
+
+const lion_params_rint_fixed_t LION_PARAMS_DEFAULT_RINT_FIXED = {.internal_resistance = 0.12};
+
+const lion_params_rint_polarization_t LION_PARAMS_DEFAULT_RINT_POLARIZATION = {
     // Charge
     .c40 =
         {
@@ -106,6 +98,26 @@ const lion_params_rint_polarization_t LION_PARAMS_RINT_DEFAULT_POLARIZATION = {
         },
 };
 
-lion_params_t                   lion_params_default(void) { return LION_APP_PARAMS_DEFAULT; }
-lion_params_rint_fixed_t        lion_params_default_fixed(void) { return LION_PARAMS_RINT_DEFAULT_FIXED; }
-lion_params_rint_polarization_t lion_params_default_polarization(void) { return LION_PARAMS_RINT_DEFAULT_POLARIZATION; }
+const lion_params_rint_t LION_PARAMS_DEFAULT_RINT = {
+    .model        = LION_RINT_MODEL_FIXED,
+    .params.fixed = LION_PARAMS_DEFAULT_RINT_FIXED,
+};
+
+const lion_params_t LION_PARAMS_DEFAULT = {
+    .init = LION_PARAMS_DEFAULT_INIT,
+    .ehc  = LION_PARAMS_DEFAULT_EHC,
+    .ocv  = LION_PARAMS_DEFAULT_OCV,
+    .vft  = LION_PARAMS_DEFAULT_VFT,
+    .temp = LION_PARAMS_DEFAULT_TEMP,
+    .rint = LION_PARAMS_DEFAULT_RINT,
+};
+
+lion_params_init_t              lion_params_default_init(void) { return LION_PARAMS_DEFAULT_INIT; }
+lion_params_ehc_t               lion_params_default_ehc(void) { return LION_PARAMS_DEFAULT_EHC; }
+lion_params_ocv_t               lion_params_default_ocv(void) { return LION_PARAMS_DEFAULT_OCV; }
+lion_params_vft_t               lion_params_default_vft(void) { return LION_PARAMS_DEFAULT_VFT; }
+lion_params_temp_t              lion_params_default_temp(void) { return LION_PARAMS_DEFAULT_TEMP; }
+lion_params_rint_fixed_t        lion_params_default_rint_fixed(void) { return LION_PARAMS_DEFAULT_RINT_FIXED; }
+lion_params_rint_polarization_t lion_params_default_rint_polarization(void) { return LION_PARAMS_DEFAULT_RINT_POLARIZATION; }
+lion_params_rint_t              lion_params_default_rint(void) { return LION_PARAMS_DEFAULT_RINT; }
+lion_params_t                   lion_params_default(void) { return LION_PARAMS_DEFAULT; }
