ddf21f5ffd6240708fac518d4a3ec95a7e39bdb0
Cristóbal Allendes <callendes.molina@gmail.com>
2025-01-19 23:00:02 -0300

    Commented the vcpkg stuff out

--- cmake/Vcpkg.cmake
+++ cmake/Vcpkg.cmake
@@ -1,13 +1,13 @@
 # TODO: Analyze the proper way to handle this scenario
-if(NOT EXISTS "${CMAKE_BINARY_DIR}/vcpkg.cmake")
-    message(
-    STATUS
-        "Downloading `vcpkg.cmake` from https://github.com/microsoft/vcpkg..."
-    )
-    file(DOWNLOAD "https://github.com/microsoft/vcpkg/raw/master/scripts/buildsystems/vcpkg.cmake"
-    "${CMAKE_BINARY_DIR}/vcpkg.cmake"
-    )
-    message(STATUS "Vcpkg config downloaded succesfully.")
-endif()
+# if(NOT EXISTS "${CMAKE_BINARY_DIR}/vcpkg.cmake")
+#     message(
+#     STATUS
+#         "Downloading `vcpkg.cmake` from https://github.com/microsoft/vcpkg..."
+#     )
+#     file(DOWNLOAD "https://github.com/microsoft/vcpkg/raw/master/scripts/buildsystems/vcpkg.cmake"
+#     "${CMAKE_BINARY_DIR}/vcpkg.cmake"
+#     )
+#     message(STATUS "Vcpkg config downloaded succesfully.")
+# endif()
 
-set(CMAKE_TOOLCHAIN_FILE "${CMAKE_TOOLCHAIN_FILE}" "${CMAKE_BINARY_DIR}/vcpkg.cmake")
+# set(CMAKE_TOOLCHAIN_FILE "${CMAKE_TOOLCHAIN_FILE}" "${CMAKE_BINARY_DIR}/vcpkg.cmake")
