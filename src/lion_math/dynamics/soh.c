6b7511d3ce9beb52e8f46e90e764fe28722a2e24
Cristóbal Allendes <callendes.molina@gmail.com>
2025-01-30 17:11:19 -0300

    Added a TODO

--- src/lion_math/dynamics/soh.c
+++ src/lion_math/dynamics/soh.c
@@ -2,6 +2,8 @@
 
 #include <math.h>
 
+// TODO: Expand the model to include the different effects
+
 double lion_soh_next(double soh, double soc_mean, double soc_max, double soc_min, lion_params_t *params) {
   double rate = exp(log(params->soh.final_soh) / params->soh.total_cycles);
   return rate * soh;
