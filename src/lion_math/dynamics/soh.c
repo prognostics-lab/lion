4585fa3e2599a0ec51f0295b6d87333ca4ee2e9e
Cristobal Allendes <callendes.molina@gmail.com>
2025-05-05 17:43:40 -0400

    Started working on the algorithm to modify the SOH

--- src/lion_math/dynamics/soh.c
+++ src/lion_math/dynamics/soh.c
@@ -4,7 +4,20 @@
 
 // TODO: Expand the model to include the different effects
 
+double degradation_factor(double soc_mean, double soc_max, double soc_min, lion_params_degradation_element_t *table) {
+  // TODO: Implement kNN to estimate degradation factor
+  // TODO: Evaluate implementing kNN through an outside library
+  return 1.0;
+}
+
 double lion_soh_next(double soh, double soc_mean, double soc_max, double soc_min, lion_params_t *params) {
-  double rate = exp(log(params->soh.final_soh) / params->soh.total_cycles);
+  double correction   = degradation_factor(soc_mean, soc_max, soc_min, params->soh.table);
+  double total_cycles = correction * (double)params->soh.total_cycles;
+
+  // TODO: Evaluate implementing temperature correction
+
+  double rate = exp(log(params->soh.final_soh) / total_cycles);
+
+  // TODO: Implement KDE to introduce noise into the rate
   return rate * soh;
 }
